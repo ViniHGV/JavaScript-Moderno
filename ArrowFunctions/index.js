711d488b891a9a09a5e8ff17f9e9f3f21471784f
Vini <ixxvinicius@gmail.com>
2023-01-13 15:10:50 -0300

    outros modos de arrow function

--- ArrowFunctions/index.js
+++ ArrowFunctions/index.js
@@ -3,12 +3,18 @@ function normalSum(a,b){
 }
 console.log(`Soma de uma função normal: ${normalSum(5,5)}`)
 
+
 const ArrowFunction = (a,b) => {
     return a+b
 }
-
 console.log(`Arrow Function: ${ArrowFunction(9,9)}`)
 
-const subArrow = (a,b) => a-b
 
-console.log (`Subtração com arrowFunction ${subArrow(9,8)}`)
+const subArrow = (a,b) => a-b
+console.log (`Subtração com arrowFunction: ${subArrow(9,8)}`)
+
+
+const number = 20
+const dobro = n => `O dobro do numero: ${n} é ${n*2}`
+console.log(dobro(number))
+
