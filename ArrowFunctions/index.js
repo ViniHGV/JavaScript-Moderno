fb6462af89e1d138a6db56689e4631e42b4274a3
Vini <ixxvinicius@gmail.com>
2023-01-13 14:59:20 -0300

    Praticando arrow Function

---
+++ ArrowFunctions/index.js
@@ -0,0 +1,14 @@
+function normalSum(a,b){
+    return a + b
+}
+console.log(`Soma de uma função normal: ${normalSum(5,5)}`)
+
+const ArrowFunction = (a,b) => {
+    return a+b
+}
+
+console.log(`Arrow Function: ${ArrowFunction(9,9)}`)
+
+const subArrow = (a,b) => a-b
+
+console.log (`Subtração com arrowFunction ${subArrow(9,8)}`)
