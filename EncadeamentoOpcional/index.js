a6232a0e9e3bfe6d2a6340857eb41947ffd2fa22
Vini <ixxvinicius@gmail.com>
2023-01-16 13:30:45 -0300

    Criando o Encadeamento Opcional

--- EncadeamentoOpcional/index.js
+++ EncadeamentoOpcional/index.js
@@ -1,1 +1,23 @@
+const user = {
+    name: "Vinicius",
+    email: "ixxvinicius@gmail.com",
+    friends: [{
+        name: "Mary",
+        address: {
+            street: "Some Street",
+            number: 45
+        }
+    }],
+    age: 42,
+    phone: {
+        code: "+55",
+        ddd: "11",
+        number: "945211254"
+    }
 
+
+}
+
+console.log(user.friends[0])
+
+console.log(user?.friends[0]?.phone?.ddd)
